7c4bf65b8476839741ec1231e189c56ac4a8b57b
terradeda <Dave>
2015-03-30 12:20:11 -0400

    Added Second output Statement

--- Hello World/src/hello_world.java
+++ Hello World/src/hello_world.java
@@ -4,6 +4,9 @@ public class hello_world {
 	public static void main(String[] args){
 		//Output the Hello-World Message
 		System.out.println("Hello-World");
+	
+		//Second Output Line
+		System.out.println("This is a change");
 	}
 	
 }
